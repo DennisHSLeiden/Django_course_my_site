1bc18770b308752371364c274fc6b9e431349e45
DennisHSLeiden <dennisleyting@gmail.com>
2023-01-23 13:47:56 +0100

    cruciale update van MinValue naar minLength

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 
 class Author(models.Model):
@@ -7,8 +7,17 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
@@ -16,9 +25,9 @@ class Post(models.Model):
     image_name = models.CharField(max_length=50)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
-    content = models.TextField(validators=[MinValueValidator(10)])
+    content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(
-        Author, on_delete=models.SET_NULL, related_name="posts")
+        Author, null=True, on_delete=models.SET_NULL, related_name="posts")
     tags = models.ManyToManyField(Tag)
 
 
