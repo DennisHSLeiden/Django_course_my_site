6f6e17c0cc53e168070dff302c56d56368757791
DennisHSLeiden <dennisleyting@gmail.com>
2023-01-25 10:19:11 +0100

    commentary section stuffies

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Author, Tag
+from .models import Post, Author, Tag, Comment
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
@@ -7,6 +7,10 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "date", "author",) # in the admin pannel you can now see the title date and author instead of ojbect#1
     prepopulated_fields = {"slug": ("title",)}  #Making the Slug based on title
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index_page"),
-    path("all-posts", views.posts, name="all-posts_page"),
-    path("all-posts/<slug:slug>", views.post_detail, name="all-post_detail_page")
+    path("", views.StartingPageView.as_view(), name="index_page"),
+    path("all-posts", views.AllPostsView.as_view(), name="all-posts_page"),
+    path("all-posts/<slug:slug>", views.SinglePostView.as_view(), name="all-post_detail_page"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
